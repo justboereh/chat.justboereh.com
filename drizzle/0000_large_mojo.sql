c7fc6c9ef4d1fcccb2434e9ffb836a94b6c9a0c4
Boe Reh <justboereh@gmail.com>
2023-11-14 16:16:54 -0600

    change a bit

--- drizzle/0000_large_mojo.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `messages` (
-	`id` int AUTO_INCREMENT NOT NULL,
-	`text` text NOT NULL,
-	`date` datetime NOT NULL,
-	CONSTRAINT `messages_id` PRIMARY KEY(`id`)
-);
--- lib/database.schema.ts
+++ lib/database.schema.ts
@@ -1,6 +1,6 @@
 import { datetime, int, mysqlTable, text } from 'drizzle-orm/mysql-core'
 
-export const messagesSchema = mysqlTable('messages', {
+export const messagesSchema = mysqlTable('chat_messages', {
   id: int('id').autoincrement().primaryKey().notNull(),
   text: text('text').notNull(),
   user: text('text').notNull(),
